9aa416889c0fb08ca5568c2caee142d38ae82280
alphak3y <alphak3y@protonmail.com>
2023-03-07 12:47:21 -0700

    add new base contracts

---
+++ contracts/base/RangePoolFactoryStorage.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPLv3
+pragma solidity ^0.8.13;
+
+import '../interfaces/IRangePoolAdmin.sol';
+
+abstract contract RangePoolFactoryStorage {
+    IRangePoolAdmin public _owner;
+    mapping(bytes32 => address) public rangePools;
+}
