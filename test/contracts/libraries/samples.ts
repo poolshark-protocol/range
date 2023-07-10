a781f3270b9286b5d8646a8b0b09377c4873e8f2
alphak3y <alphak3y@protonmail.com>
2023-07-10 11:31:59 -0600

    tick seconds accum

--- test/contracts/libraries/samples.ts
+++ test/contracts/libraries/samples.ts
@@ -88,7 +88,7 @@ describe('Samples Library Tests', function () {
 
       await validateSample({
         secondsPerLiquidityAccum: '3402823669209384634639742406885246269808',
-        tickSecondsAccum: '1935490',
+        tickSecondsAccum: '2822760',
         averagePrice: '1461300573427867316570072651998408279850435624081',
         averageLiquidity: '7995110090085540330',
         averageTick: 887270
